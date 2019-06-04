0fcb78fc207f47c02d30c287ba108bc60ed72097
nicolechang <rock90386@gmail.com>
2019-06-04 16:55:26 +0800

    Modify default log level to debug level

--- examples/ble_central_and_peripheral/experimental/ble_app_scan_adv_test/pca10040/s132/config/sdk_config.h
+++ examples/ble_central_and_peripheral/experimental/ble_app_scan_adv_test/pca10040/s132/config/sdk_config.h
@@ -369,7 +369,7 @@
 // <4=> Debug 
 
 #ifndef BLE_BAS_CONFIG_LOG_LEVEL
-#define BLE_BAS_CONFIG_LOG_LEVEL 3
+#define BLE_BAS_CONFIG_LOG_LEVEL 4
 #endif
 
 // <o> BLE_BAS_CONFIG_INFO_COLOR  - ANSI escape code prefix.
